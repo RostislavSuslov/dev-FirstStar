86f4d03e7527c436cc936171201b57558954ce53
Rostislav <suslovros@gmail.com>
2022-05-23 10:34:17 +0300

    first commit

---
+++ dev/assets/js/global.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+    $(".fancybox").fancybox();
+    $('.nice_Select').niceSelect();
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.scrolltop:hidden').stop(true, true).fadeIn();
+        } else {
+            $('.scrolltop').stop(true, true).fadeOut();
+        }
+    });
+
+    $(function() {
+        $(".muve-top").click(function() {
+            $("html,body").animate({
+                scrollTop: $(".thetop").offset().top
+            }, "1000");
+            return false
+        })
+    })
+})
