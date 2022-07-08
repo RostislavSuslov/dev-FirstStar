a344dcbe55e0c3c132b5f2803b039f5d24aa3363
Rostislav <suslovros@gmail.com>
2022-07-08 16:39:42 +0300

    news start

--- dev/assets/js/global.js
+++ dev/assets/js/global.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
     $(".fancybox").fancybox();
     $('.nice_Select').niceSelect();
-    
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
             $('.scrolltop:hidden').stop(true, true).fadeIn();
@@ -17,5 +17,29 @@ jQuery(document).ready(function($) {
             }, "1000");
             return false
         })
+    })
+
+
+    let helper = document.querySelectorAll('.who-supported-person__name');
+    let helperArray = Array.from(helper)
+
+    helperArray.forEach(item => {
+        const itemString = item.textContent;
+        const itemAnonymous = itemString.includes('Анонімна допомога') || itemString.includes('Анонимная помощь');
+
+        let firstLetter = itemString[0];
+
+        if (itemAnonymous) {
+            const anonymous = item.closest('.who-supported-person');
+            anonymous.classList.add('anonymous');
+            anonymous.querySelector('.who-supported-person__photo').innerHTML = "?";
+
+        } else if (!itemAnonymous) {
+            const noAnonymous = item.closest('.who-supported-person');
+            if (noAnonymous.querySelector('img[src=""]')) {
+                noAnonymous.classList.add('no-photo');
+                noAnonymous.querySelector('.who-supported-person__photo').innerHTML = firstLetter;
+            }
+        }
     })
 })
